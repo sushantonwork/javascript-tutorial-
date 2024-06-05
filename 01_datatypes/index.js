84255f0fedbf4d90c68008d813380f41bbbbaac0
sushantonwork <135952119+sushantonwork@users.noreply.github.com>
2024-06-05 13:56:05 +0000

    datatypes

---
+++ 01_datatypes/index.js
@@ -0,0 +1,20 @@
+"use strict" // this will treat JS code as newer version of JS
+
+
+/* TWO documentation for JS - tc39 and mdn */
+
+/*DATA TYPES:-
+
+bigint
+boolean => true/false
+string => ""
+null => standalone value   eg.  let state = null
+undefined =>
+symbol => unique
+object
+*/ 
+
+
+console.log(typeof"sushant");  // prints the datatype
+console.log(typeof null);  // o/p- object
+console.log(typeof undefined); //  o/p - undefined
